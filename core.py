7fba035932b9a63880134af477245e21a5b28107
SriMethan <77402282+SriMethan@users.noreply.github.com>
2022-01-05 15:47:31 +0530

    Detect a mate in 1

--- core.py
+++ core.py
@@ -17,5 +17,14 @@ logger = logging.getLogger(__name__)
 board = None
 
 def getBestMove(board: shogi.Board) -> shogi.Move:
+    
+    for candidate_move in list(board.legal_moves):
+        
+        board.push(candidate_move)
+        if board.is_checkmate():
+            logging.info(f"{candidate_move} will checkmate, this is the best move")
+            return candidate_move
+        else:
+            board.pop()    
 
     return random.choice(list(board.legal_moves))
